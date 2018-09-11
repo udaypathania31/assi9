2f71b27858fed5f845606e99d84874c20306c946
udaypathania31 <42261208+udaypathania31@users.noreply.github.com>
2018-09-12 00:40:49 +0530

    Add files via upload

---
+++ Vowels.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Vowels
+{	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the string");
+		
+		String inputString = sc.nextLine();
+		
+		String newInputString = inputString.replaceAll("[AEIOUaeiou]", "");
+		
+		System.out.println("The string without vowels");
+		
+		System.out.println(newInputString);
+		
+		sc.close();
+	}
+}
---
+++ WordCount.java
@@ -0,0 +1,19 @@
+public class WordCount
+{ 
+	public static void main(String args[])
+	{ 
+		String s = "welcome to India";
+ 
+		int count = 1;
+ 
+		for (int i = 0; i < s.length() - 1; i++)
+		{
+			if ((s.charAt(i) == ' ') && (s.charAt(i + 1) != ' '))
+			{
+				count++;
+ 
+			}
+		}
+ 		System.out.println("Number of words in a string = " + count);
+	}
+}
